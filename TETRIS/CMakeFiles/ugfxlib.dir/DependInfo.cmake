4075c6cdd9c87c095eaf68165692ba08044c6966
cyz199611 <b0145131h@gmail.com>
2017-09-09 21:45:04 +0800

    Game and Library Files

---
+++ TETRIS/CMakeFiles/ugfxlib.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gdisp/gdisp.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gdisp/gdisp.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gdisp/gdisp_fonts.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gdisp/gdisp_fonts.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gdisp/mcufont/mf_encoding.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gdisp/mcufont/mf_encoding.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gdisp/mcufont/mf_font.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gdisp/mcufont/mf_font.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gdisp/mcufont/mf_justify.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gdisp/mcufont/mf_justify.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gdisp/mcufont/mf_rlefont.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gdisp/mcufont/mf_rlefont.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gdisp/mcufont/mf_scaledfont.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gdisp/mcufont/mf_scaledfont.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gdriver/gdriver.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gdriver/gdriver.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gevent/gevent.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gevent/gevent.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gfx.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gfx.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/ginput/ginput.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/ginput/ginput.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/ginput/ginput_mouse.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/ginput/ginput_mouse.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gos/gos_freertos.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gos/gos_freertos.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gtimer/gtimer.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gtimer/gtimer.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gwin/gwin.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gwin/gwin.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gwin/gwin_console.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gwin/gwin_console.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/ugfx/src/gwin/gwin_wm.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/ugfxlib.dir/Libraries/ugfx/src/gwin/gwin_wm.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Libraries/STM32F4xx_StdPeriph_Driver/inc"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Libraries/CMSIS/Include"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Libraries/CMSIS/Device/ST/STM32F4xx/Include"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Utilities/STM32_EVAL/STM32F429I-Discovery"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Utilities/STM32_EVAL/Common"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/FreeRTOSv9.0.0/FreeRTOS/Source/include"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/FreeRTOSv9.0.0/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/FreeRTOSv9.0.0/FreeRTOS/Demo/Common/include"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/ugfx_2.7"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/ugfx_2.7/src/gdisp/mcufont"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/ugfx_2.7/boards/base/STM32F429i-Discovery"
+  "code"
+  "Libraries/usr"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TETRIS/CMakeFiles/usrlib.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/usr/startup_stm32f429_439xx.S" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/usrlib.dir/Libraries/usr/startup_stm32f429_439xx.S.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Libraries/STM32F4xx_StdPeriph_Driver/inc"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Libraries/CMSIS/Include"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Libraries/CMSIS/Device/ST/STM32F4xx/Include"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Utilities/STM32_EVAL/STM32F429I-Discovery"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Utilities/STM32_EVAL/Common"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/FreeRTOSv9.0.0/FreeRTOS/Source/include"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/FreeRTOSv9.0.0/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/FreeRTOSv9.0.0/FreeRTOS/Demo/Common/include"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/ugfx_2.7"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/ugfx_2.7/src/gdisp/mcufont"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/ugfx_2.7/boards/base/STM32F429i-Discovery"
+  "code"
+  "Libraries/usr"
+  "include"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/usr/ESPL_functions.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/usrlib.dir/Libraries/usr/ESPL_functions.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/usr/ParTest.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/usrlib.dir/Libraries/usr/ParTest.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/usr/gdisp_lld_ILI9341.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/usrlib.dir/Libraries/usr/gdisp_lld_ILI9341.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/usr/ginput_lld_mouse.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/usrlib.dir/Libraries/usr/ginput_lld_mouse.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/usr/port.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/usrlib.dir/Libraries/usr/port.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/usr/system_stm32f4xx.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/usrlib.dir/Libraries/usr/system_stm32f4xx.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/Libraries/usr/timertest.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/usrlib.dir/Libraries/usr/timertest.c.o"
+  "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/code/Demo.c" "/DIST/home/lab_espl_stud13/espl_workspace/espl_demo_project/Demo_STM_CMAKE/CMakeFiles/usrlib.dir/code/Demo.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Libraries/STM32F4xx_StdPeriph_Driver/inc"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Libraries/CMSIS/Include"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Libraries/CMSIS/Device/ST/STM32F4xx/Include"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Utilities/STM32_EVAL/STM32F429I-Discovery"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/STM32F4xx_DSP_StdPeriph_Lib_V1.8.0/Utilities/STM32_EVAL/Common"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/FreeRTOSv9.0.0/FreeRTOS/Source/include"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/FreeRTOSv9.0.0/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/FreeRTOSv9.0.0/FreeRTOS/Demo/Common/include"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/ugfx_2.7"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/ugfx_2.7/src/gdisp/mcufont"
+  "/DIST/lehre/lab_espl/tech/Software/ESPToolchain/ugfx_2.7/boards/base/STM32F429i-Discovery"
+  "code"
+  "Libraries/usr"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
